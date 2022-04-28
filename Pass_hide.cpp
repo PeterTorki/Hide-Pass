cf3009737140920eecc14fe8d8b274a51827f5c2
Peter Joseph <73145539+Petertorki@users.noreply.github.com>
2022-04-28 07:28:08 +0200

    Update Pass_hide.cpp

--- Pass_hide.cpp
+++ Pass_hide.cpp
@@ -1,16 +1,15 @@
-void set_Password(char *get_in){
-    for (int i = 0; i < 19;) {
-        char ch = getch();
-        if (ch == 13) {
-            get_in[i] = '\0';
-            break;
-        }
-        else if(ch != 8){
-            get_in[i++] = ch;
-            putch('*');
-        }
-        else if(i > 0)
-            cout << "\b \b",get_in[i--] = '\0';
+void set_Password(char *get_in, int sz){
+for (int i = 0; i < sz;) {
+    char ch = getch();
+    if (ch == 13) {
+        get_in[i] = '\0';
+        break;
     }
-    cout << "\n";
+    else if(ch != 8){
+        get_in[i++] = ch;
+        putch('*');
+    }
+    else if(i > 0)
+        cout << "\b \b",get_in[i--] = '\0';
 }
+cout << "\n";
