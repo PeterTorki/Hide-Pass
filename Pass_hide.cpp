949639f2f76c0452c6a8575bf1bfe36c6bbf7ef2
Peter Joseph <73145539+Petertorki@users.noreply.github.com>
2022-04-28 16:24:38 +0200

    Update Pass_hide.cpp

--- Pass_hide.cpp
+++ Pass_hide.cpp
@@ -1,16 +1,30 @@
-// Function by Peter Joseph & Mina Magdy
+#include <bits/stdc++.h>
+#include <conio.h>
+
+using namespace std;
+// Function by Peter Joseph & Mina Magdy -> 27 / 4 / 2022 ❤️
 void set_Password(char *get_in, int sz){
-for (int i = 0; i < sz;) {
-    char ch = getch();
-    if (ch == 13) {
-        get_in[i] = '\0';
-        break;
+    for (int i = 0; i < sz;) {
+        char ch = getch();
+        if (ch == 13) {
+            get_in[i] = '\0';
+            break;
+        }
+        else if(ch != 8){
+            get_in[i++] = ch;
+            putch('*');
+        }
+        else if(i > 0)
+            cout << "\b \b",get_in[i--] = '\0';
     }
-    else if(ch != 8){
-        get_in[i++] = ch;
-        putch('*');
-    }
-    else if(i > 0)
-        cout << "\b \b",get_in[i--] = '\0';
+    cout << '\n';
 }
-cout << "\n";
+
+int main() {
+    int sz = 20;
+	char Pass[sz];
+    cout << "Please Enter the Password: ";
+    set_Password(Pass, sz);
+	cout <<"Your Password is: " << Pass;
+	return 0;
+}
